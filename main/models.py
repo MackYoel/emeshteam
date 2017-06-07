83418cb38c093ee5d86bf147de880538cd88b5d4
Yoel Zalas <mack.zalas@gmail.com>
2017-06-07 11:59:22 -0500

    change facebook picture to TextField

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ User.__str__ = str_obj
 
 class Person(User):
 
-    picture = models.CharField(max_length=255, null=True, blank=True)
+    picture = models.TextField(null=True, blank=True)
     fb_id = models.CharField(max_length=255, null=True, blank=True)
 
     def __init__(self, *args, **kwargs):
